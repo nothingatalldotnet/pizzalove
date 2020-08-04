083b340d69f3dbb93a20d8722bd05aa5a17ad331
rich <rich@nothingatall.net>
2020-08-04 18:27:55 +0100

    meh

--- wp-content/themes/pizza-love/functions/functions-cleanup.php
+++ wp-content/themes/pizza-love/functions/functions-cleanup.php
@@ -191,3 +191,20 @@
 		return $classes;
 	}
 	add_filter('body_class','wt_browser_body_class');
+
+
+	function loginLogo() {
+?>
+		<style type="text/css">
+			#login h1 a, .login h1 a {
+				background-image: url("<?php echo get_stylesheet_directory_uri().'/assets/images/logo.svg'; ?>");
+				height:65px;
+				width:320px;
+				background-size: 320px 65px;
+				background-repeat: no-repeat;
+				padding-bottom: 30px;
+			}
+	</style>
+<?php
+	}
+	add_action('login_enqueue_scripts', 'loginLogo');
