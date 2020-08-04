81430b57c8e2304cead913598f2d37b417258db3
rich <rich@nothingatall.net>
2020-08-04 17:27:20 +0100

    404

--- wp-content/themes/pizza-love/404.php
+++ wp-content/themes/pizza-love/404.php
@@ -2,8 +2,14 @@
 	get_header();
 ?>
 	<div class="content">
-		<div class="404-page padded">
-			<h2>404</h2>
+		<div class="block-cta">
+			<img src="<?php echo site_url(); ?>/wp-content/themes/pizza-love/assets/images/pizza.png">
+			<div>
+				<h2 class="title">404!</h2>
+				<?php echo get_field('404_text','option'); ?><br><br>
+				<a href="<?php echo site_url(); ?>" class="red-button">Home</a>
+			</div>
+		</div>
 <?php
 	echo '	</div>';
 	echo '</div>';
