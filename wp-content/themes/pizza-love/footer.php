521f494dcf0f8a7063e9b027f7f475c96d686940
rich <rich@nothingatall.net>
2020-08-04 14:27:20 +0100

    styles

--- wp-content/themes/pizza-love/footer.php
+++ wp-content/themes/pizza-love/footer.php
@@ -3,6 +3,9 @@
 			<a class="logo" href="#">
 				<img src="<?php echo get_stylesheet_directory_uri().'/assets/images/logo.svg'; ?>" class="site-logo" alt="PizzaLove Logo">
 			</a>
+			<div class="footer-cred">
+				Site by <a href="https://nothingatall.net" target="_blank" title="Site by nothingatall.net">nothingatall dot net</a>
+			</div>
 		</footer>
 	</body>
 <?php
