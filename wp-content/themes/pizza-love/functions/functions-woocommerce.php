6a827ed7da826b2aa74f159b7c91ac347ffd313b
rich <rich@nothingatall.net>
2020-08-05 16:28:55 +0100

    wooquick

--- wp-content/themes/pizza-love/functions/functions-woocommerce.php
+++ wp-content/themes/pizza-love/functions/functions-woocommerce.php
@@ -1,2 +1,7 @@
 <?php
 	add_filter( 'wc_add_to_cart_message_html', '__return_null' );
+
+	function woocommerce_custom_single_add_to_cart_text() {
+		return __('Buy Now', 'woocommerce'); 
+	}
+	add_filter( 'woocommerce_product_single_add_to_cart_text', 'woocommerce_custom_single_add_to_cart_text' ); 
