c007fb3654743ac329fe2cd3fba79215bb32ad41
rich <rich@nothingatall.net>
2020-08-13 16:04:04 +0100

    hm

--- wp-content/themes/pizza-love/functions/functions-woocommerce.php
+++ wp-content/themes/pizza-love/functions/functions-woocommerce.php
@@ -1,7 +1,12 @@
 <?php
-	add_filter( 'wc_add_to_cart_message_html', '__return_null' );
+	add_filter('wc_add_to_cart_message_html', '__return_null' );
 
 	function woocommerce_custom_single_add_to_cart_text() {
 		return __('Buy Now', 'woocommerce'); 
 	}
-	add_filter( 'woocommerce_product_single_add_to_cart_text', 'woocommerce_custom_single_add_to_cart_text' ); 
+	add_filter('woocommerce_product_single_add_to_cart_text', 'woocommerce_custom_single_add_to_cart_text');
+
+	function woo_declare_support() {
+		add_theme_support('woocommerce');
+	}
+	add_action('after_theme_setup', 'woo_declare_support');
