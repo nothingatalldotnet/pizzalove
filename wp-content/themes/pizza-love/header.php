9e8a3560c916f3c422d9ecbce014c5fdc91858ba
rich <rich@nothingatall.net>
2020-08-16 14:08:26 +0100

    title

--- wp-content/themes/pizza-love/header.php
+++ wp-content/themes/pizza-love/header.php
@@ -7,6 +7,7 @@
 		<script src="https://kit.fontawesome.com/bb20656c21.js"></script>
 		<link href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,500,500i,700,700i&display=swap" rel="stylesheet">
 		<link rel="icon" type="image/png" href="<?php site_url()."/favicon.ico"; ?>">
+		<title><?php echo wp_title(); ?></title>
 		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
