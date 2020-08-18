669bdb8e2c5634ec8dc1fbf7d0400f4aa213df08
rich <rich@nothingatall.net>
2020-08-18 16:22:34 +0100

    stuff

--- wp-content/themes/pizza-love/page.php
+++ wp-content/themes/pizza-love/page.php
@@ -4,11 +4,13 @@
 	$this_title = get_the_title();
 ?>
 	<div class="content">
-		<div>
-			<h2><?php echo $this_title; ?></h2>
+		<div class="block-about padded">
+			<h1><?php echo $this_title; ?></h1>
+			<div>
 <?php
 	the_content();
 ?>
+			</div>
 		</div>
 	</div>
 <?php
