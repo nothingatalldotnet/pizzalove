1fc2bcef077c7705e6bdbae5199a1ac9f303d535
rich <rich@nothingatall.net>
2020-08-18 15:23:00 +0100

    footer

--- wp-content/themes/pizza-love/woocommerce/cart/cart.php
+++ wp-content/themes/pizza-love/woocommerce/cart/cart.php
@@ -179,9 +179,8 @@ defined( 'ABSPATH' ) || exit;
 					<?php } ?>
 
 					<button type="submit" class="button" name="update_cart" value="<?php esc_attr_e( 'Update cart', 'woocommerce' ); ?>"><?php esc_html_e( 'Update cart', 'woocommerce' ); ?></button>
-					<a class="button wc-backward" href="<?php echo esc_url( apply_filters( 'woocommerce_return_to_shop_redirect', wc_get_page_permalink( 'shop' ) ) ); ?>"> <?php _e( 'Continue shopping', 'woocommerce' ) ?> </a>
 					<?php do_action( 'woocommerce_cart_actions' ); ?>
-
+					<a class="button wc-backward" href="<?php echo esc_url( apply_filters( 'woocommerce_return_to_shop_redirect', wc_get_page_permalink( 'shop' ) ) ); ?>"> <?php _e( 'Continue shopping', 'woocommerce' ) ?> </a>
 					<?php wp_nonce_field( 'woocommerce-cart', 'woocommerce-cart-nonce' ); ?>
 				</td>
 			</tr>
