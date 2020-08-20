0e46ff583f16a89cbc919878352e6bd80c880b4d
rich <rich@nothingatall.net>
2020-08-20 16:42:46 +0100

    more shit

--- wp-content/themes/pizza-love/404.php
+++ wp-content/themes/pizza-love/404.php
@@ -5,7 +5,7 @@
 		<div class="block-cta">
 			<img src="<?php echo site_url(); ?>/wp-content/themes/pizza-love/assets/images/pizza.png">
 			<div>
-				<h1 class="title">404!</h1>
+				<h1 class="title">Oh no! <span class="red">404!</span></h1>
 				<p><?php echo get_field('404_text','option'); ?></p><br><br>
 				<a href="<?php echo site_url(); ?>" class="red-button">PIZZA ME!</a>
 			</div>
