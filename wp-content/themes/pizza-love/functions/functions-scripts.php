e9cc70a0ecc099c20f5096d5a4a72404075e0c3c
rich <rich@nothingatall.net>
2020-08-22 13:49:38 +0100

    okay done yas

--- wp-content/themes/pizza-love/functions/functions-scripts.php
+++ wp-content/themes/pizza-love/functions/functions-scripts.php
@@ -1,11 +1,8 @@
 <?php
 	function enqueue_scripts() {
-		// Google Maps API
-//		wp_enqueue_script( 'gmaps-api', '//maps.googleapis.com/maps/api/js?key=AIzaSyBU_po7B-zsXyGkLaeuTsWmwbd08-pil3A', null, null, true );
-
 		wp_enqueue_script('isotope-js', get_stylesheet_directory_uri() . '/assets/js/isotope.min.js', array('jquery'), 'v' . filemtime( get_stylesheet_directory() . '/assets/js/isotope.min.js'), true);
 		wp_enqueue_script('slick-js', 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js', array('jquery'), '1.8.1', true);
-		wp_enqueue_script('custom-js', get_stylesheet_directory_uri() . '/assets/js/scripts.js', array('jquery'), 'v' . filemtime( get_stylesheet_directory() . '/assets/js/scripts.js'), true);
+		wp_enqueue_script('custom-js', get_stylesheet_directory_uri() . '/assets/js/scripts.min.js', array('jquery'), 'v' . filemtime( get_stylesheet_directory() . '/assets/js/scripts.minjs'), true);
 		wp_enqueue_style('slick-css', 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css', array(), '1.8.1', 'all');
 		wp_enqueue_style('theme-normalize', get_stylesheet_directory_uri() . '/assets/css/normalize.css', array(), 'v' . filemtime( get_stylesheet_directory() . '/assets/css/normalize.css'), 'all');
 		wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/assets/css/styles.css', array(), 'v' . filemtime( get_stylesheet_directory() . '/assets/css/style.css'), 'all');
@@ -13,9 +10,8 @@
 	}
 	add_action( 'wp_enqueue_scripts', 'enqueue_scripts');
 
-
 	function add_admin_style() {
-		wp_enqueue_style( 'pizza-admin-style', get_stylesheet_directory_uri() . '/style-admin.css', array(), 'v' . filemtime( get_stylesheet_directory() . '/style-admin.css'), 'all' );
+		wp_enqueue_style('pizza-admin-style', get_stylesheet_directory_uri() . '/style-admin.css', array(), 'v' . filemtime( get_stylesheet_directory() . '/style-admin.css'), 'all' );
 	}
 	add_action('admin_head', 'add_admin_style');
 
